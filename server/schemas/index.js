58432431d384d6d9cf6fda735ae140b434c9fc2b
rrayachhetri <rrayachhetri@gmail.com>
2022-11-22 14:12:17 -0800

    GraphQl queries with TypeDefs and resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,5 @@
+// serves as the file to collect and export other two files
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
